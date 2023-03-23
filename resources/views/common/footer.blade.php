0bb4deebe4368cdab2e6dd3acc8aa6f204f77adf
Felipe <120607893+f95f@users.noreply.github.com>
2023-03-23 15:19:34 -0300

    rodapé concluído

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,119 +1,98 @@
-<footer class="page-footer font-small unique-color-dark">
+<footer class="page-footer font-small">
 
-<div style="background-color: var(--DARK);">
-  <div class="container"> <!-- rodapé - redes sociais -->
-    <div class="row py-4 d-flex align-items-center">
+    <div style="background-color: var(--DARK);"><!-- rodapé - redes sociais -->
+    <div class="container"> 
+        <div class="row py-4 d-flex align-items-center">
 
-        <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
-            <h6 class="mb-0">Nos encontre nas redes sociais!</h6>
+            <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+                <h6 class="mb-0">Nos encontre nas redes sociais!</h6>
+            </div>
+
+            <div class="col-md-6 col-lg-7 text-center text-md-right footer-icons">
+
+                <a class="fb-ic">
+                    <i class="fab fa-facebook-f white-text mr-4"> </i>
+                </a>
+                <a class="ins-ic">
+                    <i class="fab fa-instagram white-text mr-4"> </i>
+                </a>
+                <a class="tw-ic">
+                    <i class="fab fa-twitter white-text mr-4"> </i>
+                </a>
+                <a class="gplus-ic">
+                    <i class="fab fa-youtube white-text mr-4"> </i>
+                </a>
+                <a class="li-ic">
+                    <i class="fab fa-linkedin-in white-text"> </i>
+                </a>
         </div>
 
-        <div class="col-md-6 col-lg-7 text-center text-md-right">
+        </div>
+    </div>
+    </div>
 
-            <a class="fb-ic">
-                <i class="fab fa-facebook-f white-text mr-4"> </i>
-            </a>
-            <a class="ins-ic">
-                <i class="fab fa-instagram white-text mr-4"> </i>
-            </a>
-            <a class="tw-ic">
-                <i class="fab fa-twitter white-text mr-4"> </i>
-            </a>
-            <a class="gplus-ic">
-                <i class="fab fa-youtube white-text mr-4"> </i>
-            </a>
-            <a class="li-ic">
-                <i class="fab fa-linkedin-in white-text"> </i>
-            </a>
-      </div>
+    <div class="container text-center text-md-left mt-5" id = "sitemap"> <!-- rodapé - sitemap -->
+
+        <div class="row mt-3">
+
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+                <img src="../../media/logo.png" alt="Logo Grande - Vera Napoli" height = "120"></div>
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+                    <h6 class="text-uppercase font-weight-bold">Links Úteis</h6>
+                    <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 100px;">
+                    <p>
+                    <a href="#!">Pedir  </a>
+                    </p>
+                    <p>
+                    <a href="#!">Cardápio</a>
+                    </p>
+                    <p>
+                    <a href="#!">Cadastre-se</a>
+                    </p>
+                    <p>
+                    <a href="#!">Entrar</a>
+                    </p>
+                
+            </div>
+            
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+                <h6 class="text-uppercase font-weight-bold">Sobre Nós</h6>
+                <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 100px;">
+                <p>
+                    <a href="#!">A Vera Napoli</a>
+                </p>
+                <p>
+                    <a href="#!">Contatos</a>
+                </p>
+                <p>
+                    <a href="#!">Trabalhe Conosco</a>
+                </p>
+                <p>
+                    <a href="#!">Área Restrita</a>
+                </p>
+
+            </div>
+    
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+                <h6 class="text-uppercase font-weight-bold">Horários</h6>
+                <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 100px;">
+                <p>Ter - Qui: 18h às 23h</p>
+                <p>Sex - Sab: 14h às 00h</p>
+                <p>Domingos: 18h às 00h</p>
+                <p>Feriados: 14h às 23h</p>
+
+            </div>
+
+        </div>
 
     </div>
-  </div>
-</div>
 
-<div class="container text-center text-md-left mt-5"> <!-- rodapé - sitemap -->
-
-  <div class="row mt-3">
-
-    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-
-        <img src="../../media/logo.png" alt="Logo Grande - Vera Napoli" height = "120">
-
+    <div class="footer-copyright text-center py-3" style="background-color: var(--DARK);">© 2023 Copyright:
+        <a href=""> GU3026965 @ IFSP Guarulhos</a>
     </div>
-    <!-- Grid column -->
-
-    <!-- Grid column -->
-    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-
-      <!-- Links -->
-      <h6 class="text-uppercase font-weight-bold">Products</h6>
-      <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-      <p>
-        <a href="#!">MDBootstrap</a>
-      </p>
-      <p>
-        <a href="#!">MDWordPress</a>
-      </p>
-      <p>
-        <a href="#!">BrandFlow</a>
-      </p>
-      <p>
-        <a href="#!">Bootstrap Angular</a>
-      </p>
-
-    </div>
-    <!-- Grid column -->
-
-    <!-- Grid column -->
-    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-
-      <!-- Links -->
-      <h6 class="text-uppercase font-weight-bold">Useful links</h6>
-      <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-      <p>
-        <a href="#!">Your Account</a>
-      </p>
-      <p>
-        <a href="#!">Become an Affiliate</a>
-      </p>
-      <p>
-        <a href="#!">Shipping Rates</a>
-      </p>
-      <p>
-        <a href="#!">Help</a>
-      </p>
-
-    </div>
-    <!-- Grid column -->
-
-    <!-- Grid column -->
-    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-
-      <!-- Links -->
-      <h6 class="text-uppercase font-weight-bold">Contact</h6>
-      <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-      <p>
-        <i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-      <p>
-        <i class="fas fa-envelope mr-3"></i> info@example.com</p>
-      <p>
-        <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-      <p>
-        <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-
-    </div>
-    <!-- Grid column -->
-
-  </div>
-  <!-- Grid row -->
-
-</div>
-<!-- Footer Links -->
-
-<!-- Copyright -->
-<div class="footer-copyright text-center py-3">© 2020 Copyright:
-  <a href="/"> MDBootstrap.com</a>
-</div>
-<!-- Copyright -->
 
 </footer>
