bdb2c8dd58ddefaceab556f9cc063d7f43f3df24
CTI Guarulhos <ctiguarulhos@ifsp.edu.br>
2023-03-30 20:36:46 -0300

    aula 08

--- resources/views/common/nav.blade.php
+++ resources/views/common/nav.blade.php
@@ -1,38 +1,43 @@
 <header>
-        <nav class="navbar navbar-expand-md fixed-top scrolling-navbar">
-            <div class="container">
-                <a class="navbar-brand" href="index">
-                    <img src="{{asset('resources/media/logomarca_branco.png')}}" alt="Logo - Vera Napoli" height = "60">
-                    <span class="sr-only">(current)</span><!-- sr-only: texto somente disponível para leitores de tela, não fica visível -->
-                </a>
 
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7" aria-controls="navbarSupportedContent-7" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    <nav class="navbar navbar-expand-md fixed-top scrolling-navbar">
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent-7">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a class="nav-link waves-effect waves-dark" href="pedir">Pedir</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link waves-effect waves-dark" href="cardapio">Cardápio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link waves-effect waves-dark" href="sobre">Sobre</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link waves-effect waves-dark" href="contato">Contato</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link waves-effect waves-dark" href="login">Login</a>
-                        </li>
-                    </ul>
-                </div>
+        <div class="container">
 
+            <a class="navbar-brand" href="index">
+                <img src="{{asset('resources/media/logomarca_branco.png')}}" alt="Logo - Vera Napoli" height = "60">
+                <span class="sr-only">(current)</span>
+            </a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7" aria-controls="navbarSupportedContent-7" aria-expanded="false" aria-label="Toggle navigation">
+
+                <span class="navbar-toggler-icon" style = "background-color: var(--WHITE)"></span>
+            
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent-7">
+                <ul class="navbar-nav ml-auto">
+                    
+                    <li class="nav-item">
+                        <a class="nav-link waves-effect waves-dark" href="pedir">Pedir</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link waves-effect waves-dark" href="cardapio">Cardápio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link waves-effect waves-dark" href="sobre">Sobre</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link waves-effect waves-dark" href="contato">Contato</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link waves-effect waves-dark" href="login">Login</a>
+                    </li>
+
+                </ul>
             </div>
-        </nav>
 
+        </div>
+    </nav>
 
-    </div>
 </header>
