0c6a0140a8e59f0a0de21c929feef7a7c10f0497
Felipe <120607893+f95f@users.noreply.github.com>
2023-04-02 16:16:45 -0300

    ajustes finais

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,12 +1,17 @@
 <div class="mdb-bg-video banner">
+
     <video autoplay muted loop id="video-background">
         <source src="{{asset('resources/media/banner.mp4')}}" type="video/mp4">
     </video>
+
     <div class="mask pattern-8"></div>
+
     <div class="mdb-bg-video-content">
         
         <div class="row py-5 mt-3">
+
             <div class="col-md-12 wow fadeIn mb-3" style="visibility: visible; animation-name: fadeIn; animation-iteration-count: 1;">
+
                 <div class="text-center">
 
                     <img src="{{asset('resources/media/logo_branco.png')}}" alt="Logo Grande - Vera Napoli" class = "mx-auto mt-2 mb-4  wow fadeInUp"> 
@@ -24,14 +29,16 @@
                         Onde Tradição encontra a Inovação e a Criatividade
                     
                     </h5>
+
                     <div class="wow fadeInUp" data-wow-delay="0.4s" style="visibility: visible; animation-name: fadeInUp; animation-iteration-count: 1; animation-delay: 0.4s;">
+
                         <a class="btn py-4 waves-effect waves-light btn-destaque" href = "pedir"><i class="fas fa-concierge-bell"></i> Peça Agora!</a>
                         <a class="btn py-4 waves-effect waves-light btn-neutro" href="cardapio"><i class="fas fa-book left"></i> Cardápio</a>
+
                     </div>
-        
+                    
                 </div>
             </div>
         </div>
-
     </div>
 </div>
