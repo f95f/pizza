c385e3371c7164eef5ddffeb5a23440386fc7b75
f95f <95felipefernandes@gmail.com>
2023-06-29 01:07:32 -0300

    translated to PT-BR

---
+++ vera-napoli/resources/views/components/application-mark.blade.php
@@ -0,0 +1,1 @@
+<img src="{{ asset('/media/logo_branco.png') }}" style = "width: 64px" alt="Vera Napoli">
---
+++ vera-napoli/resources/views/components/authentication-card-logo.blade.php
@@ -0,0 +1,5 @@
+<a href="/">
+
+    <img src="{{ asset('/media/logomarca-vertical-branco.png') }}" style = "width: 200px" alt="Vera Napoli - Home">
+
+</a>
